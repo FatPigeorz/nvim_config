ecf0c5c1ecd747166a600c7145cf5d57e0bb74f4
fatpigeon <wjhhhhhha0@gmail.com>
2022-08-31 19:16:38 +0800

    add plugin: closer

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -66,7 +66,7 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
 --use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
---use 'rstacruz/vim-closer' -- automatically closes brackets on pressing enter
+  use 'rstacruz/vim-closer' -- automatically closes brackets on pressing enter
 
 
   -- Lazy loading:
