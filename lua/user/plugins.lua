56234c3114ea85499232846f735eaa2179f8d6c3
fatpigeon <wjhhhhhha0@gmail.com>
2022-09-01 13:35:32 +0800

    add treesitter and nvim-tree

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -118,6 +118,21 @@ return packer.startup(function(use)
     end
   }
 
+  -- Treesitter
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+  }
+
+  -- NvimTree
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icons
+    },
+    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
